f6e207848d4e7dd3ff87f5a2b5c203e05df6e469
Lenson Merwin Fernandes <lenson.fernand@gmail.com>
2023-02-15 09:08:08 +0530

    Solution updated 4

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   const fam = [
-    {id: "relativeListItem2", name: 'Lee'},
-    {id: "relativeListItem1", name: 'Jack'},
-    {id: "relativeListItem3", name: 'Sparrow'}
+    { id: "relativeListItem2", name: "Lee" },
+    { id: "relativeListItem1", name: "Jack" },
+    { id: "relativeListItem3", name: "Sparrow" },
   ];
 
   return (
     <div id="main">
-               {/* Do not remove the main div */}
-               <ol>
-        {fam.map((ele) => <li key={ele.id}>{ele.name}</li>)}
+      <ol id="relativeList">
+        <li id="relativeListItem1">Ben</li>
+        <li id="relativeListItem2">John</li>
       </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
