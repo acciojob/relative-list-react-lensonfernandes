c6004ef4950a56b056017000115b9f20191778ad
Lenson Merwin Fernandes <lenson.fernand@gmail.com>
2023-02-15 08:23:21 +0530

    Solution updated 2

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
-               <ol key={"relativeList"}>
-                  <li key={"relativeListItem1"}>lee</li>
-                  <li key={"relativeListItem2"}>Jack</li>
-                  <li key={"relativeListItem3"}>Sparrow</li>
+               <ol key="relativeList">
+                  <li key="relativeListItem1">lee</li>
+                  <li key="relativeListItem2">Jack</li>
+                  <li key="relativeListItem3">Sparrow</li>
 
                </ol>
     </div>
